e12758e998f25c6428dabf00c071094bb61e6a67
srujan <srujan.gowda@s.amity.edu>
2025-03-04 11:13:06 +0530

    added even file

--- even-odd/odd.c
+++ even-odd/odd.c
@@ -1,7 +1,18 @@
+
 int main() {
     int num;else {
         printf("%d is odd.\n", num);
     }
 
     return 0;
-}
+int main() {
+    int num;
+
+    // Asking for input
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    // Checking if the number is even or odd
+    if (num % 2 == 0) {
+        printf("%d is even.\n", num);
+    }}
